bed0f25e47de0390e5a32da499af7ca978220e4d
javierp621194 <javierp621194@gmail.com>
2022-10-17 10:12:34 -0700

    Basic beginning construction of perceptron functionality.

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,10 +1,14 @@
-import numpy
-import scipy.spatial
+import numpy as np
+from scipy.spatial import distance
 
 
 def perceptron_train(X, Y):
-    return (0, 0)
-
+    update_flag = 0
+    print("\nperceptron", X, Y, sep='\n', end='\n\n')
+    while update_flag:
+        update_flag = 0
+    temp = (0, 0)
+    return temp
 
 def perceptron_test(X_test, Y_test, w, b):
     pass
